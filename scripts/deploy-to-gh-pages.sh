78a5bbe99adf62d1798452686648ec6fde9c0432
Henry <henry1943@163.com>
2016-08-10 08:16:55 +0800

    Add echos

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -19,8 +19,14 @@ git config --global user.name "Travis CI"
 make
 
 # deploy
+echo Deploying...
 cd build
 git init
 git add .
+echo Committing...
 git commit -m "Deploy to Github Pages"
+echo Changes have been commited.
+
+echo Pushing to gh-pages...
 git push --force "https://799432ade3f4259d2e527ba39485f72001170ad8@github.com/magiccube/x-challenge-club.git" master:gh-pages > /dev/null 2>&1
+echo All assets have been updated to date in gh-pages.
