88e0cd68505d9dbac1fe93fd5656084361e2f4e2
Henry <henry1943@163.com>
2016-08-10 08:24:45 +0800

    update

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -26,7 +26,8 @@ git add .
 echo Committing...
 git commit -m "Deploy to Github Pages"
 echo Changes have been commited.
+git status
 
 echo Pushing to gh-pages...
-git push --force "https://799432ade3f4259d2e527ba39485f72001170ad8@github.com/magiccube/x-challenge-club.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://799432ade3f4259d2e527ba39485f72001170ad8@github.com/magiccube/x-challenge-club.git" gh-pages
 echo All assets have been updated to date in gh-pages.
